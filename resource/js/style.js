62779b940892b7684544d946169e5c3ddb1d48c4
eusup-chungchy <167936045+eusup@users.noreply.github.com>
2025-02-24 15:01:08 +0900

    Add files via upload

--- resource/js/style.js
+++ resource/js/style.js
@@ -1,8 +1,22 @@
 $(document).ready(function () {
+    
+    // 메뉴 클릭시
+    $("header nav li .depth").click(function(){
+        $("header nav ul li").removeClass("act");
+        $(this).closest("li").addClass("act");
+        $(this).closest(".nav2depth").closest("li").addClass("act");
+    });
+
     // 캘린더에서 일자 선택시
     $(".calendar>ul.cell>li button").click(function(){
         $(this).parents("li").addClass("act");
         $(this).parents("li").siblings("li").removeClass("act");
+    });
+
+    // 캘린더 팝업 오픈
+    $(".slide-controller .num").click(function(){
+        popUpOpen();
+        $(".popup.calendar").addClass("act");
     });
 
     // 캘린더 팝업에서 월 선택시
@@ -17,28 +31,79 @@ $(document).ready(function () {
         $(".popup.letter").addClass("act");
     });
 
-    // 탭기능
-    $(".tab-in-tab ul li button").click(function(){
-        $(this).parents("li").addClass("act");
-        $(this).parents("li").siblings("li").removeClass("act");
-    });
-
-    // 모바일 메뉴 오픈
-    $(".m-icn-menu").click(function(){
-        popUpOpen();
-        $(this).toggleClass("act");
-        $(".popup.m-header").toggleClass("act");
-    });
-
-    $(".slide-controller .num").click(function(){
-        popUpOpen();
-        $(".popup.calendar").addClass("act");
-    });
-
     // 팝업창 닫기
     $(".popup .close").click(function(){
         popUpOpen();
         $(".dimmed .popup").removeClass("act");
+    });
+
+    // 쪽지 팝업 쪽지보내기
+    $(".popup.letter .list .write").click(function(){
+        $(".popup.letter .list").removeClass("act");
+        $(".popup.letter .write").addClass("act");
+    });
+
+    // 쪽지 팝업 write에서 취소
+    $(".popup.letter .write .btn-wrap .btn:last-child").click(function(){
+        $(".popup.letter .write").removeClass("act");
+        $(".popup.letter .list").addClass("act");
+    });
+
+    // 쪽지 팝업 쪽지읽기
+    $(".popup.letter table tr").click(function(){
+        $(".popup.letter .list").removeClass("act");
+        $(".popup.letter .read").addClass("act");
+    });
+
+    // 쪽지 팝업 read에서 목록창으로
+    $(".popup.letter .read .btn-wrap .btn:first-child").click(function(){
+        $(".popup.letter .read").removeClass("act");
+        $(".popup.letter .list").addClass("act");
+    });
+
+    // 쪽지 팝업 read에서 탑장
+    $(".popup.letter .read .btn-wrap .btn:last-child").click(function(){
+        $(".popup.letter .read").removeClass("act");
+        $(".popup.letter .write").addClass("act");
+    });
+
+    // 모바일 메뉴 오픈
+    $(".m-icn-menu").click(function(){
+        $(this).toggleClass("act");
+        $(".m-header").toggleClass("act");
+    });
+
+    // 탭기능
+    $(".tab-in-tab ul li button, .tab ul li button").click(function(){
+        $(this).parents("li").addClass("act");
+        $(this).parents("li").siblings("li").removeClass("act");
+    });
+
+    // 동영상 플레이 버튼 클릭시
+    $(".video-wrap video").click(function(){
+        const videoNow = $('video').get(0);
+        if(videoNow.paused == true){
+            videoNow.play();
+            $(this).siblings(".btn").addClass("hide");
+            $(this).siblings(".btn").removeClass("show");
+        }else{
+            videoNow.pause();
+            $(this).siblings(".btn").addClass("show");
+            $(this).siblings(".btn").removeClass("hide");
+        }
+    });
+
+    // 음성파일 플레이버튼
+    $(".audio-wrap .c-audio .btn-toggle").click(function(){
+        $(this).find("button").toggleClass("act");
+    });
+
+    // 자주묻는질문 li 질문 클릭시
+    $(".qna .contents-wrap .inner>ul li .btn").click(function(){
+        $(this).parents("li").addClass("act");
+        $(this).parents("li").siblings("li").removeClass("act");
+        $(this).parents("li").siblings("li").find(".answer").slideUp();
+        $(this).siblings(".answer").slideDown();
     });
 });
 
